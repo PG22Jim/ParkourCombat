563bc4e79e3e2b4139ac84a08555cd2bd0b25985
JimChen\YuJuChen <PG22Jim@vfs.com>
2023-04-27 22:30:32 -0700

    Initialize UE5.1.1 project

---
+++ Source/ParkourCombat/ParkourCombat.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ParkourCombat.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ParkourCombat, "ParkourCombat" );
+ 
---
+++ Source/ParkourCombat/ParkourCombatGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ParkourCombatGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AParkourCombatGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AParkourCombatGameMode();
+};
+
+
+
